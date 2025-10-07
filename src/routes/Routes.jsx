1adcf78f6c5102220b4ffc2ed09997d4bb9476e0
Yolanda <yolandaalfonsolopez@gmail.com>
2025-10-07 12:29:42 +0200

    FP-23 feat: routes created

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,7 @@ export default function AppRoutes () {
     return (
         <Routes>
           <Route path="/" element={<Home />} />
+         {/*  <Route path="/" element={<Home />} /> */}
           
           </Routes>  
     )
